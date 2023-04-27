4264bea43da0d058098ba85b099addaf78a92a02
SergayKataev1 <nimnul010@yandex.ru>
2023-04-27 10:44:19 +0300

    добавлина тепезация

--- sleight_of_hand.py
+++ sleight_of_hand.py
@@ -4,8 +4,9 @@ from typing import List, Tuple
 def get_max_points(matrix: List[str], k: int) -> int:
     points = 0
     count = [0] * 16
+    dot = '.'
     for number in matrix:
-        if number != '.':
+        if number != dot:
             count[int(number)] += 1
     for number in count:
         if 0 < number <= k * 2:
