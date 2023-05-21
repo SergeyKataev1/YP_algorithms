cb4b1fae26da1562bb510b5f800830b76cc8064f
Sergey_Kataev <nimnul010@yandex.ru>
2023-05-21 09:54:16 +0300

    првки сломанного массива

--- 1_broken_search.py
+++ 1_broken_search.py
@@ -1,34 +1,30 @@
 def broken_search(nums, target):
-    left = 0
-    right = len(nums) - 1
-    while left <= right:
-        midl = (left + right) // 2
-        if nums[midl] == target:
-            return midl
-        elif nums[left] <= nums[midl]:
-            if nums[left] <= target < nums[midl]:
-                right = midl - 1
+    start_index = 0
+    end_index = len(nums) - 1
+
+    while start_index <= end_index:
+        mid_index = start_index + ((end_index - start_index) >> 1)
+
+        if nums[mid_index] == target:
+            return mid_index
+
+        if nums[start_index] <= nums[mid_index]:
+            if nums[start_index] <= target < nums[mid_index]:
+                end_index = mid_index - 1
             else:
-                left = midl + 1
+                start_index = mid_index + 1
         else:
-            if nums[midl] < target <= nums[right]:
-                left = midl + 1
+            if nums[mid_index] < target <= nums[end_index]:
+                start_index = mid_index + 1
             else:
-                right = midl - 1
+                end_index = mid_index - 1
     return -1
 
 
-def read_input():
-    _ = int(input())
-    k = int(input())
-    nums = list(map(int, input().split()))
-    return k, nums
-
-
-def main():
-    k, nums = read_input()
-    print(broken_search(nums, k))
+def test():
+    arr = [19, 21, 100, 101, 1, 4, 5, 7, 12]
+    assert broken_search(arr, 5) == 6
 
 
 if __name__ == '__main__':
-    main()
+    test()
