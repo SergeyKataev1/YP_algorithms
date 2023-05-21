35cfbdb7178392ac2100d63b259ede6d868df930
Sergey_Kataev <nimnul010@yandex.ru>
2023-05-21 10:03:35 +0300

    ID

--- 1_broken_search.py
+++ 1_broken_search.py
@@ -1,23 +1,24 @@
+#87544339
 def broken_search(nums, target):
     start_index = 0
     end_index = len(nums) - 1
 
     while start_index <= end_index:
-        mid_index = start_index + ((end_index - start_index) >> 1)
+        midl_index = start_index + ((end_index - start_index) >> 1)
 
-        if nums[mid_index] == target:
-            return mid_index
+        if nums[midl_index] == target:
+            return midl_index
 
-        if nums[start_index] <= nums[mid_index]:
-            if nums[start_index] <= target < nums[mid_index]:
-                end_index = mid_index - 1
+        if nums[start_index] <= nums[midl_index]:
+            if nums[start_index] <= target < nums[midl_index]:
+                end_index = midl_index - 1
             else:
-                start_index = mid_index + 1
+                start_index = midl_index + 1
         else:
-            if nums[mid_index] < target <= nums[end_index]:
-                start_index = mid_index + 1
+            if nums[midl_index] < target <= nums[end_index]:
+                start_index = midl_index + 1
             else:
-                end_index = mid_index - 1
+                end_index = midl_index - 1
     return -1
 
 
